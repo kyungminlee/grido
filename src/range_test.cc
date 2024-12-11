610aa85fae2d27ce3c768a43391024a7e64f6ac6
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2024-12-11 01:14:46 -0800

    Switch unit testing to doctest
    Boost::ut seems to have issue with clang on linux. Also c++20 is not the only target

--- src/range_test.cc
+++ src/range_test.cc
@@ -2,44 +2,50 @@
 #include "algorithm.hh"
 #include <iostream>
 // #include "test.hh"
-#include <boost/ut.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
 using namespace grido;
 
-int main() {
-  static_assert(std::random_access_iterator<integer_as_iterator<int>>, "int as an interator");
-  static_assert(std::random_access_iterator<integer_as_iterator<std::size_t>>, "std::size_t as an interator");
+static_assert(std::random_access_iterator<integer_as_iterator<int>>, "int as an interator");
+static_assert(std::random_access_iterator<integer_as_iterator<std::size_t>>, "std::size_t as an interator");
+
+TEST_CASE("range") {
 
   basic_integer_range<int> b(5);
-
-  boost::ut::expect(b.size() == 5);
-
-  std::cout << "b = " << b << '\n';
-  for (auto const & i: b) {
-    std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
-  }
-
-  unit_integer_range u(b);
-  std::cout << "u = " << u << '\n';
-  for (auto const & i: u) {
-    std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
-  }
-
-  {
-    integer_range<int> ir(0, 5, -2);
-    std::cout << "ir = " << ir << '\n';
-    for (auto const & i: ir) {
-      std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
-    }
-  }
-
-  {
-    integer_range<int> ir(0, -5, -2);
-    std::cout << "ir = " << ir << '\n';
-    for (auto const & i: ir) {
-      std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
-    }
-  }
-
+  REQUIRE_EQ(b.size(), 5);
 
 }
+
+
+//   // boost::ut::expect(b.size() == 5);
+
+//   std::cout << "b = " << b << '\n';
+//   for (auto const & i: b) {
+//     std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
+//   }
+
+//   unit_integer_range u(b);
+//   std::cout << "u = " << u << '\n';
+//   for (auto const & i: u) {
+//     std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
+//   }
+
+//   {
+//     integer_range<int> ir(0, 5, -2);
+//     std::cout << "ir = " << ir << '\n';
+//     for (auto const & i: ir) {
+//       std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
+//     }
+//   }
+
+//   {
+//     integer_range<int> ir(0, -5, -2);
+//     std::cout << "ir = " << ir << '\n';
+//     for (auto const & i: ir) {
+//       std::cout << DeclTypeName<decltype(i)>()() << ": " << i << "\n";
+//     }
+//   }
+
+
+// }
